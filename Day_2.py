5a6804419c544176bd5fa9e596d601bd0969b46f
daveymclain <braveheart_52@hotmail.com>
2025-05-06 16:45:17 +0100

    init

--- Day_2.py
+++ Day_2.py
@@ -1,6 +1,11 @@
+from os import remove
+
 import Tools
 import aocd
 
+
+
+PASS = 3
 with open('inputs/session.txt') as f:
     session = f.read()
 
@@ -12,7 +17,7 @@ test = """7 6 4 2 1
 1 3 6 7 9"""
 
 data = Tools.input_as_lines(aocd.get_data(session, day=2))
-
+# data = Tools.input_as_lines(test)
 
 def convert_int(d):
     temp = []
@@ -26,40 +31,45 @@ def convert_int(d):
 
 data = convert_int(data)
 
-def check_gaps(d, p):
-    remove_list = []
-    for i, line in enumerate(d):
-        increase = None
-        line_length = len(line)
-        for ii in range(line_length):
-            if ii < line_length - 1:
-                dif = int(line[ii]) - int(line[ii + 1])
-                if abs(dif) == 0:
-                    # print("fail same")
-                    remove_list.append(i)
-                    break
-                if abs(dif) <= p:
-                    if increase == None:
-                        increase = dif > 0
-                    else:
-                        temp_dir = dif > 0
-                        if increase != temp_dir:
-                            # print("fail change dir")
-                            remove_list.append(i)
-                            break
+
+def check_line(line):
+    increase = None
+    line_length = len(line)
+    for ii in range(line_length):
+        if ii < line_length - 1:
+            dif = int(line[ii]) - int(line[ii + 1])
+            if 0 < abs(dif) <= PASS:
+                if increase == None:
+                    increase = dif > 0
                 else:
-                    # print("fail number to big")
-                    remove_list.append(i)
-                    break
-    remove_list.reverse()
-    for i in remove_list:
-        d.pop(i)
+                    temp_dir = dif > 0
+                    if increase != temp_dir:
+                        # print("fail change dir")
+                        return ii
+            else:
+                return ii
+    return True
 
-    return d
+total = 0
+for i, line in enumerate(data):
+    length = len(line)
+    print("line to test " + str(line))
+    attempt = False
+    result = check_line(line)
+    if type(result) == int:
+        for i in range(length):
+            test = line.copy()
+            test.pop(i)
+            r = check_line(test)
+            if type(r) == int:
+                print("not working" + str(i))
+            else:
+                print("worked with " + str(i))
+                total += 1
+                break
+    else:
+        print("pass")
+        total += 1
+print(total)
 
 
-PASS = 3
-safe = 0
-
-data = check_gaps(data,PASS)
-print(len(data))
